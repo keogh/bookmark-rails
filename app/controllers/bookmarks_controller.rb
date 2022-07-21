48367eab287dbacf76acb249fa30e9aeeea23377
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2022-07-21 15:32:53 -0600

    Add some frontend styling using tailwind

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -29,7 +29,7 @@ class BookmarksController < ApplicationController
     if @bookmark.update(bookmark_params)
       redirect_to bookmarks_path, notice: "Bookmark was successfully updated."
     else
-      render :edit, , status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
