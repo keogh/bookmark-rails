7058fd97cbae0a4cd93cce9e8123e2c2f3d2a3ae
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2022-08-01 16:57:34 -0600

    Add index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :bookmarks
-
-  root to: "bookmarks#index"
 end
